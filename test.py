4cc0f8601f9ce2d9901baaaf7717289198c515c1
joel <joellopes153@gmail.com>
2023-12-02 16:49:54 -0300

    novas test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-number = range(0,11)
+import random
+
+number = list(range(0,11))
+number= random.choice(number)
 print(number)
-second_number = range(0,11)
+second_number = random.choice(range(0,11))
 if number == second_number:
     print(f'aleatorios nao aleatorios kkk {second_number}')
